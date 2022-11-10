e78b9f48487656a2fe4f19b7af07a75f508abf05
chae-rim <108618066+chae-rim@users.noreply.github.com>
2022-11-10 23:17:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,3 +16,48 @@ $(function () {
 
 
 });
+
+
+
+
+
+
+// skill
+
+$(function () {
+    var scene01Num = 1;
+
+    $(window).scroll(function () {
+        var scrollTop = $(document).scrollTop();
+        if (scrollTop > 1800 && scene01Num == 1) {
+            scene01Num = 0;
+            console.log('ok')
+            $('#skill .box1').stop().delay(0).css({ right: '400px' })
+                .animate({ right: '100px' }, 1000);
+            $('#skill .box2').stop().delay(500).css({ right: '400px' })
+                .animate({ right: '150px' }, 1000);
+            $('#skill .box3').stop().delay(1000).css({ right: '400px' })
+                .animate({ right: '350px' }, 1000);
+            $('#skill .box4').stop().delay(1500).css({ right: '400px' })
+                .animate({ right: '350px' }, 1000);
+        }
+        else if (scrollTop < 1500 && scene01Num == 0) {
+            scene01Num = 1;
+            $('#skill .box1').stop().css({ right: '400px' })
+                .animate({ right: '100px' }, 1000);
+            $('#skill .box2').stop().css({ right: '400px' })
+                .animate({ right: '150px' }, 1000);
+            $('#skill .box3').stop().css({ right: '400px' })
+                .animate({ right: '350px' }, 1000);
+            $('#skill .box4').stop().css({ right: '400px' })
+                .animate({ right: '350px' }, 1000);
+        }
+    });
+});
+
+
+
+
+
+
+// project
