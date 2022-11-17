6048caa6697b1152300bae6933435b8c08dbde0f
chae-rim <108618066+chae-rim@users.noreply.github.com>
2022-11-17 16:33:04 +0900

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.nav li a').mouseenter(function(){
+        $(this).css({color : 'white' })
+    })
+    $('.nav a').mouseleave(function(){
+        $(this).css({color : 'black'})
+    })
+})
